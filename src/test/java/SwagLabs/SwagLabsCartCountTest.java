ad2af9d3e0337e722ccaa2aab4f32735b18a3161
JSmith200 <yk380506909406@gmail.com>
2021-09-15 09:53:30 +0300

    Refactoring test

--- src/test/java/SwagLabs/SwagLabsCartCountTest.java
+++ src/test/java/SwagLabs/SwagLabsCartCountTest.java
@@ -5,19 +5,15 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
-public class SwagLabsTest extends SwagSettings {
+public class SwagLabsCartCountTest extends SwagSettings {
 
     @Test
-    public void Test1() {
+    public void cartCount() {
         WebElement body = driver.findElement(By.tagName("body"));
         body.findElement(By.id("login-button")).click();
         body.findElement(By.id("add-to-cart-test.allthethings()-t-shirt-(red)")).click();
-        body.findElement(By.xpath("//*[@id=\"header_container\"]/div[2]/div[2]/span/select/option[3]")).click();
-
-        String sortingBy = body.findElement(By.xpath("//*[@id=\"header_container\"]/div[2]/div[2]/span/span")).getText();
         String cartCount = body.findElement(By.xpath("//*[@id=\"shopping_cart_container\"]/a/span")).getText();
 
-        Assert.assertEquals("PRICE (LOW TO HIGH)", sortingBy);
         Assert.assertEquals("1", cartCount);
     }
 }
